0579965f18303db74cab8367a7da36b28a3ca65f
kre8mymedia <kre8mymedia@gmail.com>
2023-11-22 23:59:47 -0600

    chat g2g

--- promptengineers/controllers/__init__.py
+++ promptengineers/controllers/__init__.py
@@ -1,9 +1,11 @@
 from .history import HistoryController
 from .auth import AuthController
 from .storage import StorageController
+from .chat import ChatController
 
 __all__ = [
 	'HistoryController',
 	'AuthController',
-	'StorageController'
+	'StorageController',
+	'ChatController',
 ]
