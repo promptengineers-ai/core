366a5fc901e0090ab5cd795181411561ad8461d8
kre8mymedia <kre8mymedia@gmail.com>
2024-01-20 23:13:19 -0600

    Updates FastAPI package

--- promptengineers/models/request/retrieval/__init__.py
+++ promptengineers/models/request/retrieval/__init__.py
@@ -1,22 +1,13 @@
 """Request Models"""
-from typing import Any, List, Optional, Union
+from typing import List
 from pydantic import BaseModel, Field
-
-from promptengineers.models.loader import (
-    TypeUrlLoader,
-    YoutubeLoader,
-    BlockchainLoader,
-    CopyPasteLoader,
-)
 
 class RequestMultiLoader(BaseModel):
     index_name: str = Field(...)
     provider: str = ("pinecone", "redis")
     embedding: str = ("text-embedding-ada-002", "llama2:7b", "llama2")
     files: List[str] or None = Field(...)
-    loaders: List[
-        Union[TypeUrlLoader, YoutubeLoader, BlockchainLoader, CopyPasteLoader]
-    ] or None = Field(...)
+    loaders: List[dict] or None = Field(...)
 
     class Config:
         json_schema_extra = {
@@ -39,9 +30,7 @@ class RequestDataLoader(BaseModel):
     index_name: str = Field(...)
     provider: str = ("pinecone", "redis")
     embedding: str = ("text-embedding-ada-002", "llama2:7b", "llama2")
-    loaders: List[
-        Union[TypeUrlLoader, YoutubeLoader, BlockchainLoader, CopyPasteLoader]
-    ] or None = Field(...)
+    loaders: List[dict] or None = Field(...)
 
     class Config:
         json_schema_extra = {
