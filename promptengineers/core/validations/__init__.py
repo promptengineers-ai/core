89f1685b28f7ad69ebc25adfe30f7206659ed107
Ryan Eggleston <40816745+ryaneggz@users.noreply.github.com>
2024-03-09 11:19:34 -0600

    MERGE development INTO master (#96)
    * Adds multi-tenancy to retrieval factory. May need to rethink how package is laid out
    
    * Update changelog
    
    * Turns everything to async
    
    * PXT app mostly updated
    
    * Agent Chat, Rag Chat, RetrievalFactory, and Embedding refactored (#86)
    
    * Update changelog
    
    * Update README.md
    
    Remove user_repo exerpt now that detaching has been made from controllers and user_repo
    
    * MERGE feature/87-cleans-up-document-loading-functionality INTO development (#88)
    
    * Simpler use of the loading documents and cleaned up
    
    * clean up
    
    * Update changelog
    
    * Updates FastAPI package
    
    * Updates for default chat
    
    * Fixes to disallowed_special on OpenAIEmbeddings, and optional tokens passed to retreive pinecone
    
    * Resolved deprecations and warnings
    
    * UPdate changelog
    
    * Add handle_parsing_errors
    
    * Fixes file upload
    
    * updates from new model releases
    
    * updates changelog
    
    * MERGE bugfix/remove-singleton-from-mongo-service INTO development (#92)
    
    * MERGE development INTO master (#90)
    
    * Adds multi-tenancy to retrieval factory. May need to rethink how package is laid out
    
    * Update changelog
    
    * Turns everything to async
    
    * PXT app mostly updated
    
    * Agent Chat, Rag Chat, RetrievalFactory, and Embedding refactored (#86)
    
    * Update changelog
    
    * Update README.md
    
    Remove user_repo exerpt now that detaching has been made from controllers and user_repo
    
    * MERGE feature/87-cleans-up-document-loading-functionality INTO development (#88)
    
    * Simpler use of the loading documents and cleaned up
    
    * clean up
    
    * Update changelog
    
    * Updates FastAPI package
    
    * Updates for default chat
    
    * Fixes to disallowed_special on OpenAIEmbeddings, and optional tokens passed to retreive pinecone
    
    * Resolved deprecations and warnings
    
    * UPdate changelog
    
    * Add handle_parsing_errors
    
    * Fixes file upload
    
    * MERGE development INTO master (#91)
    
    * Adds multi-tenancy to retrieval factory. May need to rethink how package is laid out
    
    * Update changelog
    
    * Turns everything to async
    
    * PXT app mostly updated
    
    * Agent Chat, Rag Chat, RetrievalFactory, and Embedding refactored (#86)
    
    * Update changelog
    
    * Update README.md
    
    Remove user_repo exerpt now that detaching has been made from controllers and user_repo
    
    * MERGE feature/87-cleans-up-document-loading-functionality INTO development (#88)
    
    * Simpler use of the loading documents and cleaned up
    
    * clean up
    
    * Update changelog
    
    * Updates FastAPI package
    
    * Updates for default chat
    
    * Fixes to disallowed_special on OpenAIEmbeddings, and optional tokens passed to retreive pinecone
    
    * Resolved deprecations and warnings
    
    * UPdate changelog
    
    * Add handle_parsing_errors
    
    * Fixes file upload
    
    * updates from new model releases
    
    * Update changelog
    
    * Adds default tokens for files
    
    * Update changelog
    
    * Needed a fix
    
    * removes singleton, causes issues
    
    * MERGE feature/94-application-token-defaults INTO development (#95)
    
    * Allows for default application tokens
    
    * Update changelog
    
    * resolved conflicts

--- promptengineers/core/validations/__init__.py
+++ promptengineers/core/validations/__init__.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, List, Set
 
 from promptengineers.core.exceptions import ValidationException
 from promptengineers.core.exceptions.messages import token_not_found_message
@@ -9,11 +9,11 @@ class Validator:
 	def validate_api_keys(
 		self,
 		available_tokens: Union[dict, str],
-		require_keys: list[str],
+		require_keys: Union[List[str], Set[str]],
 		exception_class=ValidationException
 	) -> None:
 		if isinstance(available_tokens, str):
-			available_tokens = {require_keys[0]: available_tokens}
-		error_message = token_not_found_message(available_tokens, require_keys)
+			available_tokens = {require_keys[0]: available_tokens}  # This assumes require_keys is not empty
+		error_message = token_not_found_message(available_tokens, list(require_keys))
 		if error_message:
 			raise exception_class(error_message)
