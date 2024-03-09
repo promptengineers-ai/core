fdf8ae303a2e06757d15fa0cb4b6f2949ce4a7a5
Ryan Eggleston <40816745+ryaneggz@users.noreply.github.com>
2024-03-09 10:33:12 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
             'langchain==0.0.354',
             'langchain-openai', 
             'openai', 
+            'langsmith==0.0.77',
         ],
         'mongo': [
             'motor', 
